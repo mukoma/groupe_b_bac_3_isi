33b1d7e9d10b3375d49c2503d01132988da4bdcd
ngonga3 <gadlumbwe8@gmail.com>
2022-07-09 13:43:01 +0200

    Ajout de la page SECTION et une petit modification de propietes de la header page

---
+++ section.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>section</title>
+        <link rel="stylesheet" type="text/css" href="Styles_tp.css">
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <section class="conteneur">
+            <div class="gallery">
+                <a target="_blank" href="img_5terre.jpg">
+                <img src="cuivre.jpg" alt="Cinque Terre" width="300" height="100">
+                </a>
+                <div class="desc">Cuivre d'origine Africaine en RDC..</div>
+            </div>
+
+            <div class="gallery">
+                <a target="_blank" href="img_forest.jpg">
+                <img src="diam.jpg" alt="Forest" width="300" height="100">
+                </a>
+                <div class="desc">Diamant de production de gisement de bukavu rd-Congo...</div>
+            </div>
+
+            <div class="gallery">
+                <a target="_blank" href="img_lights.jpg">
+                <img src="diam1.jpg" alt="zing" width="300" height="100">
+                </a>
+                <div class="desc">Diamant de production de gisement de bukavu rd-Congo...</div>
+            </div>
+
+            <div class="gallery">
+                <a href="https://cdn.futura-sciences.com/buildsv6/images/mediumoriginal/4/c/d/4cd8fe98ca_50063356_4098-cuivre-302a.jpg">
+                <img src="min.jpg" alt="Mountains" width="300" height="100">
+                </a>
+                <div class="desc">Le zinc est un métal diamagnétique blanc bleuâtre, 
+                    brillant ...</div>
+            </div>
+            
+            <div>
+            <div class="gallery">
+                <a target="_blank" href="img_5terre.jpg">
+                <img src="or.jpg" alt="Cinque Terre" width="300" height="100">
+                </a>
+                <div class="desc">Or pure issue de gisment de KINKOLE dans le KATANGA rd-CONGO</div>
+            </div>
+
+            <div class="gallery">
+                <a target="_blank" href="img_forest.jpg">
+                <img src="or1.jpg" alt="Forest" width="300" height="100">
+                </a>
+                <div class="desc">LINGO Or de la générale carière de mines</div>
+            </div>
+
+            <div class="gallery">
+                <a target="_blank" href="img_lights.jpg">
+                <img src="or.jpg" alt="zing" width="300" height="100">
+                </a>
+                <div class="desc">Or des sites miniers de Kolwezi rd-Congo</div>
+            </div>
+
+            <div class="gallery">
+                <a target="_blank" href="img_mountains.jpg">
+                <img src="c1.jpg" alt="Mountains" width="300" height="100">
+                </a>
+                <div class="desc">le fer une extraction du Lualaba </div>
+            </div>
+            </div><br>
+            <center>
+                <button class="ps-img"> plus d'images </button>
+            </center><br>
+
+        </section>
+    </body>
+</html>
